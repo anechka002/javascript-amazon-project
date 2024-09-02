3f59aa45616118533fe0ba7c5357eead390246a2
Anna <blinova.anneta@gmail.com>
2024-09-02 14:29:46 +0200

    Save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,26 @@
-export let cart = [{
-  productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-  quantity: 2
-}, {
-  productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-  quantity: 1
-}];
+export let cart = JSON.parse(localStorage.getItem('cart')) || [
+  {
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 2
+  }, {
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 1
+  }
+];
+// if(!cart) {
+//   cart = [{
+//     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+//     quantity: 2
+//   }, {
+//     productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+//     quantity: 1
+//   }];
+// }
+
+// функция для сохранения корзины в localStorage
+function saveToStorage () {
+  localStorage.setItem('cart', JSON.stringify(cart))
+}
 
 // функция по добавлению в корзину
 export function addToCart (productId) {
@@ -20,6 +36,8 @@ export function addToCart (productId) {
   } else {
     cart.push({productId: productId, quantity: 1})
   }
+
+  saveToStorage();
 }
 
 export function removeFromCart (productId) {
@@ -31,4 +49,6 @@ export function removeFromCart (productId) {
     }
   })
   cart = newCart;
+
+  saveToStorage();
 }
