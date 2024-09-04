118cd59cb822791f01fdcf1f13fc82a02a2df288
Anna <blinova.anneta@gmail.com>
2024-09-04 13:02:19 +0200

    Make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,6 +3,7 @@ import {products, getProduct} from '../../data/products.js';
 import {formatCurrency} from '../utils/money.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js'
+import { renderPaymentSummary } from './paymentSummary.js';
 
 // библиотека dayjs
 const today = dayjs();
@@ -119,6 +120,7 @@ export function renderOrderSummary () {
       const container = document.querySelector(`.js-cart-item-container-${productId}`);
       container.remove()
 
+      renderPaymentSummary()
       updateCartQuantity()
     })
   });
@@ -165,6 +167,7 @@ export function renderOrderSummary () {
         const {productId, deliveryOptionId} = element.dataset;
         updateDeliveryOptions(productId, deliveryOptionId);
         renderOrderSummary();
+        renderPaymentSummary();
       })
     })
 }
