2c502f6214bcd756ceb2e0921e5af6323fe85b0b
Anna <blinova.anneta@gmail.com>
2024-09-06 11:52:10 +0200

    Create automated tests for addToCart

--- data/cart.js
+++ data/cart.js
@@ -1,23 +1,30 @@
-export let cart = JSON.parse(localStorage.getItem('cart')) || [
-  {
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 2,
-    deliveryOptionId: '1'
-  }, {
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 1,
-    deliveryOptionId: '2'
-  }
-];
-// if(!cart) {
-//   cart = [{
-//     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-//     quantity: 2
-//   }, {
-//     productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-//     quantity: 1
-//   }];
-// }
+export let cart;
+
+localFromStorage();
+
+// функция загрузить из хранилища
+export function localFromStorage () {
+  cart = JSON.parse(localStorage.getItem('cart')) || [
+    {
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 2,
+      deliveryOptionId: '1'
+    }, {
+      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      quantity: 1,
+      deliveryOptionId: '2'
+    }
+  ];
+  // if(!cart) {
+  //   cart = [{
+  //     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+  //     quantity: 2
+  //   }, {
+  //     productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+  //     quantity: 1
+  //   }];
+  // }
+}
 
 // функция для сохранения корзины в localStorage
 function saveToStorage () {
