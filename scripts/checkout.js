0cd60f82f9bd502fa710f23ab19793dd2322c47c
Anna <blinova.anneta@gmail.com>
2024-09-10 15:00:55 +0200

    Create function loadCartFetch and use to loadPage

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,7 +2,7 @@ import {renderCheckoutHeader} from './checkout/checkoutHeader.js';
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
 import { loadProductsFetch } from '../data/products.js';
-import { loadCart } from '../data/cart.js';
+import { loadCartFetch } from '../data/cart.js';
 // import '../data/cart-oop.js'
 // import '../data/cart-class.js'
 // import '../data/backend-practice.js'
@@ -12,14 +12,8 @@ async function loadPage () {
     // throw 'error1'
 
     await loadProductsFetch();
-  
-    const value = await new Promise((resolve, reject) => {
-      // throw 'error2'
-      loadCart(() => {
-        // reject('error3')
-        resolve('value3');
-      });
-    });
+
+    await loadCartFetch();
 
   } catch (error) {
     console.log('Unexpected error. Please try again later.')
