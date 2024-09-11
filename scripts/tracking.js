2fd318303ab69fafc14c7ab61be22e9f864a0e74
Anna <blinova.anneta@gmail.com>
2024-09-11 10:03:09 +0200

    Make the tracking page interactive

--- scripts/tracking.js
+++ scripts/tracking.js
@@ -19,6 +19,18 @@ async function loadPage () {
     }
   })
 
+  const today = dayjs();
+  const orderTime = dayjs(order.orderTime);
+  const deliveryTime = dayjs(productDetails.estimatedDeliveryTime);
+  // const percentProgress = ((today - orderTime) / (deliveryTime - orderTime)) * 100;
+  const percentProgress = Math.min(Math.max(((today - orderTime) / (deliveryTime - orderTime)) * 100, 0), 100);
+
+
+  console.log('Today:', today.format());
+console.log('Order Time:', orderTime.format());
+console.log('Delivery Time:', deliveryTime.format());
+console.log('Percent Progress:', percentProgress);
+
   const trackingHTML = `   
     <a class="back-to-orders-link link-primary" href="orders.html">
       View all orders
@@ -39,19 +51,19 @@ async function loadPage () {
     <img class="product-image" src="${product.image}">
 
     <div class="progress-labels-container">
-      <div class="progress-label">
+      <div class="progress-label ${percentProgress < 50 ? 'current-status' : ''}">
         Preparing
       </div>
-      <div class="progress-label current-status">
+      <div class="progress-label ${(percentProgress >= 50 && percentProgress < 100) ? 'current-status' : ''}">
         Shipped
       </div>
-      <div class="progress-label">
+      <div class="progress-label ${percentProgress >= 100 ? 'current-status' : ''}">
         Delivered
       </div>
     </div>
 
     <div class="progress-bar-container">
-      <div class="progress-bar"></div>
+      <div class="progress-bar style=width: ${percentProgress}%;"></div>
     </div>  
   `;
 
